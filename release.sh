2a2db2e722c70d1f833267b0694bd4997bb71dfb
eric <ericlam@vitessedata.com>
2023-10-10 16:24:03 +0100

    update release script

--- release.sh
+++ release.sh
@@ -1,24 +1,31 @@
-#!/bin/bash
-
-if [ $# != 1 ]; then
-    echo "usage: $0 version"; echo
+if [ $# -eq 0 ]; then
+    echo "usage: $0 version [branch]"; echo
     exit 1
 fi
 
-url=$(git config --get remote.origin.url)
-
+BRANCH="main"
 VERSION=$1
 S3POOL=s3pool-$VERSION
+if [ $# -eq 2 ]; then
+BRANCH=$2
+echo "BRANCH=$BRANCH"
+fi
+
 echo $S3POOL
-
 rm -rf $S3POOL $S3POOL.zip
-
+url=$(git config --get remote.origin.url)
 git clone $url $S3POOL
+if [ $BRANCH != "main" ] ; then
+echo "checkout $BRANCH"
+(cd $S3POOL && git checkout $BRANCH)
+fi
 
-find $S3POOL -name .gitignore -delete
-rm -rf $S3POOL/.git* 
-rm -rf $S3POOL/release.sh 
+find $S3POOL -name '*.xrg' -delete
+find $S3POOL -name '*.csv' -delete
+find $S3POOL -name '*.csv.gz' -delete
+rm -rf $S3POOL/.git*
+rm -rf $S3POOL/release.sh
 rm -rf $S3POOL/tests
-
 zip -r $S3POOL.zip $S3POOL
 rm -rf $S3POOL
+
